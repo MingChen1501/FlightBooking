6e0f764aba63f2c3667a4e35a96622c7628ebf1a
MinhTran <tminh400@gmail.com>
2024-04-09 03:10:51 +0700

    Initial commit

---
+++ src/test/java/org/ronin/FlightBooking/FlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ronin.FlightBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
